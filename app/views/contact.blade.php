cd656f8bb189749a9e48259de68486dc52e1c282
BioDice <martijn_vankreij@hotmail.com>
2014-11-18 23:38:31 +0100

    test

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Electronix Store - Details</title>
+<title>Electronix Store - Contact</title>
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
 <link rel="stylesheet" type="text/css" href="style.css" />
 <!--[if IE 6]>
 <link rel="stylesheet" type="text/css" href="iecss.css" />
 <![endif]-->
-<script type="text/javascript" src="js/windowopen.js"></script>
 <script type="text/javascript" src="js/boxOver.js"></script>
 </head>
 <body>
@@ -31,7 +30,7 @@
           <div class="oferta_details">
             <div class="oferta_title">Samsung GX 2004 LM</div>
             <div class="oferta_text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco </div>
-            <a href="details.html" class="details">details</a> </div>
+            <a href="#" class="details">details</a> </div>
         </div>
         <div class="oferta_pagination"> <span class="current">1</span> <a href="#">2</a> <a href="#">3</a> <a href="#">4</a> <a href="#">5</a> </div>
       </div>
@@ -55,7 +54,7 @@
         <li class="divider"></li>
         <li><a href="#" class="nav5">Shipping</a></li>
         <li class="divider"></li>
-        <li><a href="contact.html" class="nav6">Contact Us</a></li>
+        <li><a href="contact.blade.php" class="nav6">Contact Us</a></li>
         <li class="divider"></li>
         <li class="currencies">Currencies
           <select>
@@ -86,8 +85,8 @@
       </ul>
       <div class="title_box">Special Products</div>
       <div class="border_box">
-        <div class="product_title"><a href="details.html">Motorola 156 MX-VL</a></div>
-        <div class="product_img"><a href="details.html"><img src="../app/images/laptop.png" alt="" border="0" /></a></div>
+        <div class="product_title">Motorola 156 MX-VL</div>
+        <div class="product_img"><a href="#"><img src="../app/images/laptop.png" alt="" border="0" /></a></div>
         <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
       </div>
       <div class="title_box">Newsletter</div>
@@ -98,55 +97,35 @@
     </div>
     <!-- end of left content -->
     <div class="center_content">
-      <div class="center_title_bar">Motorola 156 MX-VL</div>
+      <div class="center_title_bar">Contact Us</div>
       <div class="prod_box_big">
         <div class="top_prod_box_big"></div>
         <div class="center_prod_box_big">
-          <div class="product_img_big"> <a href="javascript:popImage('images/big_pic.jpg','Some Title')" title="header=[Zoom] body=[&nbsp;] fade=[on]"><img src="../app/images/laptop.gif" alt="" border="0" /></a>
-            <div class="thumbs"> <a href="#" title="header=[Thumb1] body=[&nbsp;] fade=[on]"><img src="../app/images/thumb1.gif" alt="" border="0" /></a> <a href="#" title="header=[Thumb2] body=[&nbsp;] fade=[on]"><img src="../app/images/thumb1.gif" alt="" border="0" /></a> <a href="#" title="header=[Thumb3] body=[&nbsp;] fade=[on]"><img src="../app/images/thumb1.gif" alt="" border="0" /></a> </div>
-          </div>
-          <div class="details_big_box">
-            <div class="product_title_big">My Cinema-U3000/DVBT, USB 2.0 TV BOX External, White</div>
-            <div class="specifications"> Disponibilitate: <span class="blue">In stoc</span><br />
-              Garantie: <span class="blue">24 luni</span><br />
-              Tip transport: <span class="blue">Mic</span><br />
-              Pretul include <span class="blue">TVA</span><br />
+          <div class="contact_form">
+            <div class="form_row">
+              <label class="contact"><strong>Name:</strong></label>
+              <input type="text" class="contact_input" />
             </div>
-            <div class="prod_price_big"><span class="reduce">350$</span> <span class="price">270$</span></div>
-            <a href="#" class="addtocart">add to cart</a> <a href="#" class="compare">compare</a> </div>
+            <div class="form_row">
+              <label class="contact"><strong>Email:</strong></label>
+              <input type="text" class="contact_input" />
+            </div>
+            <div class="form_row">
+              <label class="contact"><strong>Phone:</strong></label>
+              <input type="text" class="contact_input" />
+            </div>
+            <div class="form_row">
+              <label class="contact"><strong>Company:</strong></label>
+              <input type="text" class="contact_input" />
+            </div>
+            <div class="form_row">
+              <label class="contact"><strong>Message:</strong></label>
+              <textarea class="contact_textarea" ></textarea>
+            </div>
+            <div class="form_row"> <a href="#" class="contact">send</a> </div>
+          </div>
         </div>
         <div class="bottom_prod_box_big"></div>
-      </div>
-      <div class="center_title_bar">Similar products</div>
-      <div class="prod_box">
-        <div class="top_prod_box"></div>
-        <div class="center_prod_box">
-          <div class="product_title"><a href="details.html">Motorola 156 MX-VL</a></div>
-          <div class="product_img"><a href="details.html"><img src="../app/images/laptop.gif" alt="" border="0" /></a></div>
-          <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
-        </div>
-        <div class="bottom_prod_box"></div>
-        <div class="prod_details_tab"> <a href="#" title="header=[Add to cart] body=[&nbsp;] fade=[on]"><img src="../app/images/cart.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Specials] body=[&nbsp;] fade=[on]"><img src="../app/images/favs.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Gifts] body=[&nbsp;] fade=[on]"><img src="../app/images/favorites.gif" alt="" border="0" class="left_bt" /></a> <a href="details.html" class="prod_details">details</a> </div>
-      </div>
-      <div class="prod_box">
-        <div class="top_prod_box"></div>
-        <div class="center_prod_box">
-          <div class="product_title"><a href="details.html">Iphone Apple</a></div>
-          <div class="product_img"><a href="details.html"><img src="../app/images/p4.gif" alt="" border="0" /></a></div>
-          <div class="prod_price"><span class="price">270$</span></div>
-        </div>
-        <div class="bottom_prod_box"></div>
-        <div class="prod_details_tab"> <a href="#" title="header=[Add to cart] body=[&nbsp;] fade=[on]"><img src="../app/images/cart.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Specials] body=[&nbsp;] fade=[on]"><img src="../app/images/favs.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Gifts] body=[&nbsp;] fade=[on]"><img src="../app/images/favorites.gif" alt="" border="0" class="left_bt" /></a> <a href="details.html" class="prod_details">details</a> </div>
-      </div>
-      <div class="prod_box">
-        <div class="top_prod_box"></div>
-        <div class="center_prod_box">
-          <div class="product_title"><a href="details.html">Samsung Webcam</a></div>
-          <div class="product_img"><a href="details.html"><img src="../app/images/p5.gif" alt="" border="0" /></a></div>
-          <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
-        </div>
-        <div class="bottom_prod_box"></div>
-        <div class="prod_details_tab"> <a href="#" title="header=[Add to cart] body=[&nbsp;] fade=[on]"><img src="../app/images/cart.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Specials] body=[&nbsp;] fade=[on]"><img src="../app/images/favs.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Gifts] body=[&nbsp;] fade=[on]"><img src="../app/images/favorites.gif" alt="" border="0" class="left_bt" /></a> <a href="details.html" class="prod_details">details</a> </div>
       </div>
     </div>
     <!-- end of center content -->
@@ -184,7 +163,7 @@
     <div class="center_footer"> Template name. All Rights Reserved 2008<br />
       <a href="http://csscreme.com"><img src="../app/images/csscreme.jpg" alt="csscreme" border="0" /></a><br />
       <img src="../app/images/payment.gif" alt="" /> </div>
-    <div class="right_footer"> <a href="#">home</a> <a href="#">about</a> <a href="#">sitemap</a> <a href="#">rss</a> <a href="contact.html">contact us</a> </div>
+    <div class="right_footer"> <a href="#">home</a> <a href="#">about</a> <a href="#">sitemap</a> <a href="#">rss</a> <a href="contact.blade.php">contact us</a> </div>
   </div>
 </div>
 <!-- end of main_container -->
